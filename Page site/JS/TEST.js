a5f72d70ee7cb968377ad82b5538aa003eb1c2dd
Rudy DUCOM <rducom@geiffel.fr>
2023-03-28 13:16:33 +0200

    Version 3.1

---
+++ Page site/JS/TEST.js
@@ -0,0 +1,15 @@
+  // Inclure l'en-tête
+$(function(){
+    $("#header").load("header.html"); 
+  });
+  
+  // Inclure la barre de navigation
+  $(function(){
+    $("#nav").load("nav.html"); 
+  });
+  
+  // Inclure le pied de page
+  $(function(){
+    $("#footer").load("footer.html"); 
+  });
+  
--- Page site/test/services.php
+++ Page site/test/services.php
@@ -8,24 +8,7 @@
 		</style>
 	</head>
     <body>
-        <div id="entete">
-            <header>
-                <h1><span>Maison des ligues de Lorraine</span></h1>
-                <p><img src="Image/Logo.jfif" alt="Logo"></p>
-            </header>
-        </div>
-        
-        <nav>
-            <div>
-                <ul>
-                    <li><a href="index.html">Accueil</a></li>
-                    <li><a href="services.html" >Services</a></li>
-                    <li><a href="ligues.html">Ligues</a></li>
-                    <li><a href="locaux.html">Locaux</a></li>
-                </ul>
-            </div> 
-        </nav>
-        
+        <?php include 'e="vertical-align: i.php'; ?>
         <section class="contenu">
             <BLOCKQUOTE>
                 <div class="titre">
